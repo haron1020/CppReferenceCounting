7816575c4b5af46ed5441446bdce566fbaa05ab0
Ilya Kulebyakin <ilya.kulebyakin@e-legion.com>
2015-10-11 12:41:16 +0300

    Initial Commit

---
+++ RefC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RefC
+//
+//  Created by Ilya Kulebyakin on 10/11/15.
+//  Copyright © 2015 Ilya Kulebyakin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
