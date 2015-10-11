494130d5d588c64e07f9d8ff47679f782056fff6
Ilya Kulebyakin <ilya.kulebyakin@e-legion.com>
2015-10-11 17:14:38 +0300

    Usage of reference counting system commented

--- RefC/main.cpp
+++ RefC/main.cpp
@@ -14,11 +14,18 @@
 
 
 int main(int argc, const char * argv[]) {
-   
+    // All of program execution which utilizes reference counting system should be wrapped in POOL_START/POOL_END macros
     POOL_START
     
+    // All constructors are required to call RootObject's constructor during thyself initializtion
     ConcreteObject *root1 = new ConcreteObject();
+    
+    // Temporary macro to make pointer safe. If at any moment during execution object, that root1 points to will be deallocated, root1 pointer will get a value of nullptr
     Safify(root1)
+    // Puts object into autoreleasepool.
+    // Reference counter is decreased by one and if it equals
+    // zero, object will be deallocated when the closest
+    // autorelease pool drains
     Autorelease(root1)
     
     ConcreteObject *weakLeaf = new ConcreteObject();
@@ -27,14 +34,19 @@ int main(int argc, const char * argv[]) {
     ConcreteObject *strongLeaf = new ConcreteObject();
     Safify(strongLeaf)
     root1->setStrong(strongLeaf);
+    
+    // Immediate object deallocation, if reference counter equals zero after 1 is subrtracted
     Release(strongLeaf)
     root1->setWeak(weakLeaf);
     {
+        // Nested pool
+        // All autoreleased objects will get into this pool
         POOL_START
     
         ConcreteObject* weak2 = new ConcreteObject();
         Safify(weak2)
         strongLeaf->setWeak(weak2);
+        // For example this one
         Autorelease(weak2)
         POOL_END
     }
